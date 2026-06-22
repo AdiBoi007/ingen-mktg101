ee11ff1d8e8e53c1f9815722a5a44e0aa0bf51e3
Prabhmannat <23bcb10048@cuchd.in>
2026-06-22 15:58:49 +0530

    Remove LiveDemo section from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import ToolMarquee from "@/components/ToolMarquee";
 import HowItWorks from "@/components/sections/HowItWorks";
-import LiveDemo from "@/components/sections/LiveDemo";
 import GlobeWaitlist from "@/components/GlobeWaitlist";
 import FAQ from "@/components/FAQ";
 import Footer from "@/components/Footer";
@@ -14,7 +13,6 @@ export default function HomePage() {
       <Hero />
       <ToolMarquee />
       <HowItWorks />
-      <LiveDemo />
       <GlobeWaitlist />
       <FAQ />
       <Footer />
